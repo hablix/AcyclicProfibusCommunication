c6e99a3be688879b0d84572ef9f95d99baa54d9c
AlsoJimCurry <jim.molkenthin@web.de>
2020-06-18 18:18:13 +0200

    Read Directory List

--- acyclic_profibus.py
+++ acyclic_profibus.py
@@ -31,7 +31,6 @@ def get_device_information(addr):
     slot = 1
     index = 0
     
-
     # Get DIRECTORY_OBJECT_HEADER
     dir_obj_header = sendMessage(framemarker, addr, slot, index)    
 
@@ -45,11 +44,51 @@ def get_device_information(addr):
     pbd.num_blocks = int.from_bytes(pbd.num_dir_entry, 'big') - int.from_bytes(pbd.num_comp_list_dir_entry, 'big')
     
     # Get COMPOSITE_LIST_DIRECTORY_ENTRIES
-    dir_entries = sendMessage(framemarker+1, addr, slot, index+1)
+    dir_entries_list = sendMessage(framemarker+1, addr, slot, index+1)
+    num_comp = int.from_bytes(pbd.num_comp_list_dir_entry, 'big')
 
+    pbd.directory = []
+
+    if num_comp >= 1:
+        pbd.directory.append({
+            "type": "Physical Block",
+            "index": dir_entries_list[1],
+            "offset": dir_entries_list[2],
+            "number": dir_entries_list[3:5]
+        })
     
+    if num_comp >= 2:
+        pbd.directory.append({
+            "type": "Transducer Block",
+            "index": dir_entries_list[5],
+            "offset": dir_entries_list[6],
+            "number": dir_entries_list[7:9]
+        })
     
-    print(f"From bus address: {addr} recieved: {dir_entries}")
+    if num_comp >= 3:
+        pbd.directory.append({
+            "type": "Function Block",
+            "index": dir_entries_list[9],
+            "offset": dir_entries_list[10],
+            "number": dir_entries_list[11:13]
+        })
+    
+    if num_comp >= 4:
+        pbd.directory.append({
+            "type": "Link Object",
+            "index": dir_entries_list[13],
+            "offset": dir_entries_list[14],
+            "number": dir_entries_list[15:17]
+        })
+    
+    # Get Composite_Directory_Entries
+    blocks_index = dir_entries_list[1]
+    dir_blocks = sendMessage(framemarker+2, addr, slot, blocks_index)
+    # TODO: Auslesen der einzelnen Blöcke. PB -> Hersteller, FB -> Funktion und Wert, TB -> Einheit
+    
+    print(pbd.directory)
+    print(f"From bus address {addr} recieved Composite_Directory_Entries: {dir_blocks}")
+    
     return pbd
 
 def sendMessage(framemarker, addr, slot, index):
